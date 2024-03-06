5666771431eca2f3554b99da2a19a4bca39757f5
Azzidahaka <Azzidahaka@gmail.com>
2024-03-06 03:05:49 -0700

    defined proptypes for moviecard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,30 @@
-export const MovieCard = ({movie, onMovieClick})=> {
-    return (<div onClick={()=>{
+import PropTypes from 'prop-types';
+
+//MovieCard components
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <div
+      onClick={() => {
         onMovieClick(movie);
-    }}>
-      {movie.title}
-    </div>)
-  };
+      }}
+    >
+      {movie.Title}
+    </div>
+  );
+};
+
+//Define props constraint for moviecard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
