441b96f5744a14a2f2937c3a19d8639368e009a4
Azzidahaka <Azzidahaka@gmail.com>
2024-03-06 03:06:20 -0700

    defined proptype

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,47 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick}) => {
   return (
     <div>
       <div>
         <img
-          src={movie.imagePath}
+          src={movie.ImagePath}
           height='400px'
         />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre.name}</span>
+        <span>{movie.Genre.Name}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director.name}</span>
+        <span>{movie.Director.Name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+ //Define props constraint for view
+ MovieView.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }).isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
 };
