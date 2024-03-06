4f756af7b9ed997c51bda12229d96ce4440df683
Azzidahaka <Azzidahaka@gmail.com>
2024-03-06 02:51:42 -0700

    created branch

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { MainView } from './components/main-view/main-view';
 
 import './index.scss';
 const MyFlixApplication = () => {
-  return (<MainView/>);
+  return (<MainView/>)
 };
 
 const container = document.querySelector('#root');
