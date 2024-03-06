cb55936370610314fe1373308a16748bc630ffdf
Azzidahaka <Azzidahaka@gmail.com>
2024-03-06 03:13:43 -0700

    added filtered and added similar movies to movieview

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+//Return bool if movie genre are the same as long as the name is not the same
+const checkMovies = (movie, selected) => {
+  return movie.Genre.Name === selected.Genre.Name && movie._id !== selected._id;
+};
 
 export const MainView = () => {
   const [movies, setMovies] = useState();
@@ -12,27 +16,41 @@ export const MainView = () => {
         setMovies(moviesFromApi);
       });
   }, []);
+
   const [selectedMovie, setSelectedMovie] = useState(null);
   if (selectedMovie) {
+    //filter movies by genre
+    let similarMovies = movies.filter((movie) => checkMovies(movie, selectedMovie));
     return (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-    );
-  } else {
-    return (
-      <div>
-        {movies.map((movie) => (
+      <>
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={() => setSelectedMovie(null)}
+        />
+        <h2>Similar Movies</h2>
+        {similarMovies.map((movie) => (
           <MovieCard
-            key={movie.id}
+            key={movie._id}
             movie={movie}
             onMovieClick={(newSelectedMovie) => {
               setSelectedMovie(newSelectedMovie);
             }}
           />
         ))}
-      </div>
+      </>
     );
   }
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie._id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
 };
